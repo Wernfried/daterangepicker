6aee5c490cf396b5f4afee64350f61c8d901a485
Wernfried <github@domscheit.ch>
2026-03-16 10:44:18 +0100

    Updated README.md

--- dist/esm/daterangepicker.js
+++ dist/esm/daterangepicker.js
@@ -362,7 +362,7 @@ class DateRangePicker {
             console.error(`Value of startDate "${this.#startDate}" violates ${vio.find((x) => x.reason.startsWith("isInvalid")).reason}`);
           } else {
             const newDate = vio.filter((x) => x.new != null).at(-1).new;
-            if (process.env.JEST_WORKER_ID == null)
+            if (process?.env.JEST_WORKER_ID == null)
               console.warn(`Correcting startDate from ${this.#startDate} to ${newDate}`);
             this.#startDate = newDate;
           }
@@ -374,7 +374,7 @@ class DateRangePicker {
               console.error(`Value of endDate "${this.#endDate}" violates ${vio.find((x) => x.reason.startsWith("isInvalid")).reason}`);
             } else {
               const newDate = vio.filter((x) => x.new != null).at(-1).new;
-              if (process.env.JEST_WORKER_ID == null)
+              if (process?.env.JEST_WORKER_ID == null)
                 console.warn(`Correcting endDate from ${this.#endDate} to ${newDate}`);
               this.#endDate = newDate;
             }
