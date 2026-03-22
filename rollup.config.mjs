9acc64f529ec4ef4bcb4766972ce5b6548aeeece
Wernfried <github@domscheit.ch>
2026-03-22 12:36:21 +0100

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,73 +1,32 @@
 import { minify } from 'rollup-plugin-esbuild-minify'
 
+let output = [];
+const formats = {
+   cjs: false,
+   esm: false,
+   umd: { globals: true, name: true },
+   iife: { globals: true, name: true }
+};
+
+for (let fmt of Object.keys(formats)) {
+   for (let compact of [true, false]) {
+      let out = {
+         file: `dist/${fmt}/daterangepicker${compact ? '.min' : ''}.js`,
+         format: fmt,
+         sourcemap: true,
+         plugins: [minify({ minify: compact })]
+      };
+      if (formats[fmt].globals)
+         out.globals = { luxon: 'luxon' };
+      if (formats[fmt].name)
+         out.name = 'DateRangePicker'
+      output.push(out);
+   }
+
+}
+
 export default {
    input: 'src/daterangepicker.js',
-   //external: ['jquery'],
    external: ['jquery', 'luxon'],
-   output: [
-      {
-         file: 'dist/cjs/daterangepicker.js',
-         format: 'cjs'
-      },
-      {
-         file: 'dist/esm/daterangepicker.js',
-         format: 'esm'
-         //plugins: [terser()]
-      },
-      {
-         file: 'dist/iife/daterangepicker.js',
-         format: 'iife',
-         globals: {
-            jquery: '$',
-            daterangepicker: 'daterangepicker',
-            luxon: 'luxon'
-         },
-         name: 'DateRangePicker'
-      },
-      {
-         file: 'dist/umd/daterangepicker.js',
-         format: 'umd',
-         globals: {
-            jquery: '$',
-            daterangepicker: 'daterangepicker',
-            luxon: 'luxon'
-         },
-         name: 'DateRangePicker'
-      },
-
-            {
-         file: 'dist/cjs/daterangepicker.min.js',
-         format: 'cjs',
-         compact: true
-      },
-      {
-         file: 'dist/esm/daterangepicker.min.js',
-         format: 'esm',
-         compact: true,
-         plugins: [minify({minify:true})]
-      },
-      {
-         file: 'dist/iife/daterangepicker.min.js',
-         format: 'iife',
-         globals: {
-            jquery: '$',
-            daterangepicker: 'daterangepicker',
-            luxon: 'luxon'
-         },
-         name: 'DateRangePicker',
-         compact: true
-      },
-      {
-         file: 'dist/umd/daterangepicker.min.js',
-         format: 'umd',
-         globals: {
-            jquery: '$',
-            daterangepicker: 'daterangepicker',
-            luxon: 'luxon'
-         },
-         name: 'DateRangePicker',
-         compact: true
-      }
-
-   ]
+   output: output
 };
